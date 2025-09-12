82e9317d9e64cd8342502f7e66b02dd79417c24d
Bruno Lalomia <421489@tecnicatura.frc.utn.edu.ar>
2025-09-12 18:13:07 -0300

    Ignorar repositorio embebido Actividades-Practicas

---
+++ WebAPIProyecto/Controllers/BillController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Practica01_.Domain;
+
+namespace WebAPIProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillController : ControllerBase
+    {
+        static readonly List<Bill> lst = new List<Bill>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            lst.Add(new Bill() { Id=1});
+            return Ok(lst);
+        }
+    }
+}
